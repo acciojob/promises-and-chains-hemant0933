3b008c908eff291e540b311037828445291ccb9a
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-12 13:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+var age =  document.getElementById('age').value
+var name = document.getElementById('Name').value
+var submitbtn = document.getElementById('submitBtn').addEventListener('click',()=>{
+		if(age === '' || name === ''){
+		alert('please fill the field')
+		}
+		else{
+			return new Promise(resolve,reject)=>{
+				if(age > 18){
+					resolve(
+					setTimeout(()=>{
+						alert('Welcome,'+ `${name}`+'. You can vote.')
+					}() , 4000))
+				}
+				else{
+					reject(alert("Oh sorry"+ `${name}`+". You aren't old enough."))
+				}
+			}
+		}
+})
