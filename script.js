4447b067d719a5d9d00b9a1f477dd8fdccc0144d
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-16 10:38:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 //your JS code here. If required.
-var age =  document.getElementById('age').value
-var name = document.getElementById('Name').value
-var submitbtn = document.getElementById('submitBtn').addEventListener('click',()=>{
-		if(age === '' || name === ''){
-		alert('please fill the field')
-		}
-		else{
-			return new Promise(resolve,reject)=>{
-				if(age > 18){
-					resolve(
-					setTimeout(()=>{
-						alert('Welcome,'+ `${name}`+'. You can vote.')
-					}() , 4000))
-				}
-				else{
-					reject(alert("Oh sorry"+ `${name}`+". You aren't old enough."))
-				}
-			}
-		}
-})
+const form = document.querySelector('form');
+const ageInput = document.querySelector('#age');
+const nameInput = document.querySelector('#name');
+const submitBtn = document.querySelector('#btn');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  // Check if inputs are empty
+  if (ageInput.value === '' || nameInput.value === '') {
+    alert('Please fill in all fields');
+    return;
+  }
+  
+  // Get age and name from input values
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value;
+
+	const agePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+  
+  // Handle the promise
+  agePromise
+    .then((message) => {
+      alert(message);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+  
+  // Clear the inputs
+  ageInput.value = '';
+  nameInput.value = '';
+});
+
+
